137bdaecc99dda871277bf0405dce5a6a5436adf
Rjburghardt <rjburghardt@gmail.com>
2015-10-23 10:30:28 -0700

    Initial commit for twitter-v1

---
+++ main.js
@@ -0,0 +1,63 @@
+$(function() {
+  var user = {
+    handle: '@bradwestfall',
+    img: '/images/brad.png'
+  }
+
+  var composeTemplate = $('#template-compose').text();
+  var composeCompile = Handlebars.compile(composeTemplate);
+
+  var renderCompose = function () {
+    return composeCompile() // this may need to be changed back to with out the parens
+  }
+  
+  var tweetTemplate = $('#template-tweet').text()
+  var tweetCompile = Handlebars.compile(tweetTemplate) //NO need to compile the code each time we call the function
+
+  var renderTweet = function (user, message) {
+    return tweetCompile({ 
+        userimg: user.img, 
+        userhandle: user.handle,
+        message: message})
+  };
+
+  var threadTemplate = $('#template-thread').text();
+  var threadCompile = Handlebars.compile(threadTemplate);
+
+  var renderThread = function (user, message) {
+    $('.tweets').append(threadCompile({
+      tweetAdd: renderTweet(user, message),
+      compose: renderCompose()
+    }));
+  }
+
+  $('main').on('click', '.compose textarea', function () {
+    $(this).parent('.compose').addClass('expand');
+  });
+
+  $('.tweets').on('click', '.tweet', function () {
+    $(this).parent('.thread').toggleClass('expand');
+  });
+
+  $('main').on('submit', 'form.compose', function () {
+    var message = $(this).find('textarea').val()
+    
+    $(this).removeClass('expand');
+    $(this).find('div').find('.count').text(140);
+    $(this).find('textarea').val('');
+    
+    if ($(this).parent('header').length){
+      renderThread(user, message);
+    } else {
+      $(this).parents('.replies').append(renderTweet(user, message));
+    }
+    
+    return false
+
+  });
+
+  $('main').on('keyup', 'textarea', function () {
+    var value = $(this).val().length;
+    $(this).next('div').find('.count').text(140 - value);
+  });
+});
